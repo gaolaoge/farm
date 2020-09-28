cc70c9863e9aa3b77221542a7224b2f06a1e4b5c
gaoge <22334455>
2020-09-28 15:13:11 +0800

    update

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,13 +2,13 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_URI: '"http://192.168.1.179"',              // 开发
-  BASE_URI: '"http://223.80.107.190:18146"',    // 测试环境
+  // BASE_URI: '"http://223.80.107.190:18146"',    // 测试环境
   // BASE_URI: '"http://10.2.2.146"',              // 测试环境2
-  // BASE_URI: '"http://223.80.107.190:28146"',                  // 生产
+  BASE_URI: '"www.chinamoviecloud.cn"',                  // 生产
   // BACK_WS_API: '"ws://192.168.1.176:5002/websocket/web/"',      // 开发
-  BACK_WS_API: '"ws://223.80.107.190:5002/websocket/web/"',  // 测试环境
+  // BACK_WS_API: '"ws://223.80.107.190:5002/websocket/web/"',  // 测试环境
   // BACK_WS_API: '"ws://10.2.2.142:5002/websocket/web/"',      // 测试环境2
-  // BACK_WS_API: '"ws://223.80.107.190:5003/websocket/web/"',         // 生产
+  BACK_WS_API: '"ws://223.80.107.190:5003/websocket/web/"',         // 生产
   PLUGIN_WS_API: '"ws://localhost:15000"',
   // MOCKJS: false
 }
