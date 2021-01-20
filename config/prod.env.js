b52da889f522779b09aa260b8d69606848c07a0c
高歌 <gaog@yzhj.com>
2021-01-20 09:24:14 +0800

    update

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-<<<<<<< HEAD
   BASE_URI: '"http://223.80.107.190:18146"',        // 测试环境
   // BASE_URI: '"http://10.2.2.146"',               // 测试环境2
   BACK_WS_API: '"ws://223.80.107.190:5002/websocket/web/"',        // 测试环境
@@ -9,13 +8,5 @@ module.exports = {
   DOWNLOAD_PLUGIN_PATH: '"http://223.80.107.190:8084/CloudTransfer.exe"',                // 开发、测试
   PLUGIN_WS_API: '"ws://localhost:17002"',
   MOCKJS: false,
-  VERSION: 202101061546,
-=======
-  BASE_URI: '"http://192.168.1.179"',            // 开发
-  BACK_WS_API: '"ws://192.168.1.176:5002/websocket/web/"',      // 开发
-  DOWNLOAD_PLUGIN_PATH: '"http://223.80.107.190:8084/CloudTransfer.exe"',                // 开发、测试
-  PLUGIN_WS_API: '"ws://localhost:17002"',
-  MOCKJS: false,
-  VERSION: 202101061611,
->>>>>>> dev-2.0.1
+  VERSION: 202101200923
 }
