f92a3d75e737b28bfc086f974afbab5b9657d4b6
高歌 <gaog@yzhj.com>
2021-02-03 10:10:55 +0800

    update

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,12 +4,10 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URI: '"http://223.80.107.190:18146"',        // 测试环境
-  // BASE_URI: '"http://10.2.2.146"',               // 测试环境2
-  BACK_WS_API: '"ws://223.80.107.190:5002/websocket/web/"',        // 测试环境
-  // BACK_WS_API: '"ws://10.2.2.142:5002/websocket/web/"',         // 测试环境2
-  DOWNLOAD_PLUGIN_PATH: '"http://223.80.107.190:8084/CloudTransfer.exe"',                // 开发、测试
+  BASE_URI: '"http://www.chinamoviecloud.cn:28146"',            // 生产
+  BACK_WS_API: '"ws://223.80.107.190:5003/websocket/web/"',     // 生产
+  DOWNLOAD_PLUGIN_PATH: '"http://www.chinamoviecloud.cn:8085/CloudTransfer.exe"',     // 生产
   PLUGIN_WS_API: '"ws://localhost:17002"',
-  MOCKJS: true,
-  VERSION: 202101301115
+  MOCKJS: false,
+  VERSION: 202102031010
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -4,7 +4,7 @@ module.exports = {
   BASE_URI: '"http://www.chinamoviecloud.cn:28146"',            // 生产
   BACK_WS_API: '"ws://223.80.107.190:5003/websocket/web/"',     // 生产
   DOWNLOAD_PLUGIN_PATH: '"http://www.chinamoviecloud.cn:8085/CloudTransfer.exe"',     // 生产
-  PLUGIN_WS_API: '"ws://localhost:15000"',
+  PLUGIN_WS_API: '"ws://localhost:17002"',
   MOCKJS: false,
-  VERSION: 202101251109,
+  VERSION: 202102031010
 }
